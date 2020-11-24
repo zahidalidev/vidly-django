d54ba36cdce87d4b9e22b93f72923652b650aeb4
zahidalidev <m.zahidalidev@gmail.com>
2020-11-25 02:24:56 +0500

    rest api created

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -12,3 +12,15 @@ def index(request):
 
     return render(request, 'movies/index.html', {'movies': movies})
     # return HttpResponse(output)
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
+
+    # or
+    # try:
+    #     movie = Movie.objects.get(id=movie_id)
+    #     return render(request, 'movies/detail.html', {'movie': movie})
+    # except Movie.DoesNotExist:
+    #     raise Http404()
--- vidly/urls.py
+++ vidly/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.models import MovieResource
+
+movies_resource = MovieResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls'))  # specify a path for movies app
+    path('movies/', include('movies.urls')),  # specify a path for movies app
+    path('api/', include(movies_resource.urls))
+
 ]
